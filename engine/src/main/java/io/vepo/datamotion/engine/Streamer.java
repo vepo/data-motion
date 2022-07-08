d12cb838eb96dbef039244d3af1ccb05712d03ea
Victor Osório <vepo@tutanota.com>
2022-07-08 16:09:00 -0300

    Tests not working

--- engine/src/main/java/io/vepo/datamotion/engine/Streamer.java
+++ engine/src/main/java/io/vepo/datamotion/engine/Streamer.java
@@ -2,17 +2,28 @@ package io.vepo.datamotion.engine;
 
 import io.vepo.datamotion.configuration.StreamerDefinition;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 public class Streamer implements Closeable {
-
+    private static final Logger logger = LoggerFactory.getLogger(Streamer.class);
     private final StreamerDefinition definition;
     private CountDownLatch latch;
     private KafkaStreams streams;
@@ -22,6 +33,7 @@ public class Streamer implements Closeable {
     }
 
     public Streamer start() {
+        logger.info("Starting Streamer! definition={}", definition);
         latch = new CountDownLatch(1);
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, definition.getApplicationId());
@@ -29,20 +41,25 @@ public class Streamer implements Closeable {
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, String> stream = builder.stream(definition.getInputTopic());
-        stream.to(definition.getOutputTopic());
+        applyDefinition(builder.stream(definition.getInputTopic()))
+                .to(definition.getOutputTopic(), Produced.with(Serdes.String(), Serdes.String()));
         streams = new KafkaStreams(builder.build(), props);
 
         Runtime.getRuntime()
                .addShutdownHook(new Thread("streams-shutdown-hook") {
                    @Override
                    public void run() {
+                       logger.info("Stopping Streamer! definition={}", definition);
                        streams.close();
                        latch.countDown();
                    }
                });
         streams.start();
         return this;
+    }
+
+    private KStream<String, String> applyDefinition(KStream<String, String> stream) {
+        return stream;
     }
 
     public void join() {
@@ -55,6 +72,10 @@ public class Streamer implements Closeable {
 
     @Override
     public void close() {
+        StringWriter sw = new StringWriter();
+        new Throwable("").printStackTrace(new PrintWriter(sw));
+        logger.info("Stack trace! stack={}", sw.toString());
+        logger.info("Request shutdown!");
         latch.countDown();
         streams.close();
     }
--- engine/src/test/java/io/vepo/datamotion/engine/StreamerTest.java
+++ engine/src/test/java/io/vepo/datamotion/engine/StreamerTest.java
@@ -1,6 +1,10 @@
 package io.vepo.datamotion.engine;
 
 import io.vepo.datamotion.configuration.StreamerDefinition;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -9,8 +13,11 @@ import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -29,24 +36,32 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 @Testcontainers
 class StreamerTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(StreamerTest.class);
     @Container
-    public KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7"));
+    public KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.5.0"));
     private static final String APP_ID = "STREAMER_TEST_APP";
 
     @BeforeEach
     void cleanup() throws IOException {
-        Files.walk(Paths.get(System.getProperty("java.io.tmpdir"), "kafka-streams", APP_ID))
-             .sorted(Comparator.reverseOrder())
-             .map(Path::toFile)
-             .forEach(File::delete);
-
+        Path tempFolder = Paths.get(System.getProperty("java.io.tmpdir"), "kafka-streams", APP_ID);
+        if (tempFolder.toFile().exists()) {
+            Files.walk(tempFolder)
+                 .sorted(Comparator.reverseOrder())
+                 .map(Path::toFile)
+                 .forEach(File::delete);
+        }
     }
 
     @Test
     void passthruTest() {
+        createTopics("input", "output");
         try (Streamer streamer = new Streamer(StreamerDefinition.builder()
                                                                 .applicationId(APP_ID)
                                                                 .inputTopic("input")
@@ -54,14 +69,28 @@ class StreamerTest {
                                                                 .bootstrapServers(kafka.getBootstrapServers())
                                                                 .build());
              TestConsumer<String, Long> consumer = start("output", StringDeserializer.class, LongDeserializer.class)) {
-            Executors.newSingleThreadExecutor().submit(streamer::start);
+            streamer.start();
             sendMessage("input", "Hello World!");
-            consumer.next((key, value) -> System.out.println("Key=" + key + " value=" + value));
+            consumer.next((key, value) -> System.out.println("Key=" + key + " value=" + value), Duration.ofSeconds(160));
+            logger.info("Test finished!");
+        }
+    }
+
+    private void createTopics(String... topics) {
+        logger.info("Creating topics! topics={}", topics);
+        List<NewTopic> newTopics =
+                Arrays.stream(topics)
+                      .map(topic -> new NewTopic(topic, 6, (short) 1))
+                      .collect(Collectors.toList());
+        try (AdminClient admin = AdminClient.create(Collections.singletonMap(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers()))) {
+            CreateTopicsResult results = admin.createTopics(newTopics);
+            logger.info("Create topic results={}", results.values());
         }
     }
 
     private <K, V> TestConsumer<K, V> start(String topic, Class<? extends Deserializer<K>> keyDeserializerClass,
                                             Class<? extends Deserializer<V>> valueDeserialzierClass) {
+        logger.info("Starting consumer...");
         Properties configProperties = new Properties();
         configProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
         configProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "Test-Consumer-" + UUID.randomUUID().toString());
@@ -80,20 +109,25 @@ class StreamerTest {
         public TestConsumer(KafkaConsumer<K, V> consumer, String topic) {
             this.consumer = consumer;
             consumer.subscribe(Arrays.asList(topic));
-            Executors.newSingleThreadExecutor().submit(() -> {
-                while (running.get()) {
-                    consumer.poll(Duration.ofMillis(500)).forEach(record -> messages.add(record));
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                }
-            });
+            Executors.newSingleThreadExecutor()
+                     .submit(() -> {
+                         while (running.get()) {
+                             consumer.poll(Duration.ofMillis(500)).forEach(record -> messages.add(record));
+                             try {
+                                 Thread.sleep(50);
+                             } catch (InterruptedException e) {
+                                 Thread.currentThread().interrupt();
+                             }
+                         }
+                     });
         }
 
-        public void next(BiConsumer<K, V> consumer) {
+        public void next(BiConsumer<K, V> consumer, Duration timeout) {
+            long start = System.nanoTime();
             while (messages.isEmpty()) {
+                if (timeout.toNanos() < System.nanoTime() - start) {
+                    fail("No message received! timeout=" + timeout);
+                }
                 try {
                     Thread.sleep(50);
                 } catch (InterruptedException e) {
@@ -112,6 +146,7 @@ class StreamerTest {
     }
 
     private void sendMessage(String topic, String message) {
+        logger.info("Sending message! topic={} message={}", topic, message);
         Properties configProperties = new Properties();
         configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
         configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -121,9 +156,9 @@ class StreamerTest {
             ProducerRecord<String, String> rec = new ProducerRecord<>(topic, message);
             Future<RecordMetadata> results = producer.send(rec);
             RecordMetadata metadata = results.get();
-            System.out.println(metadata);
+            logger.info("Message sent! metadata={}", metadata);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            fail("Error sending message!", e);
         } catch (ExecutionException e) {
             Thread.currentThread().interrupt();
         }
